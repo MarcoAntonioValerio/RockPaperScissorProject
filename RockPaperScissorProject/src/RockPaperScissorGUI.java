9cb9f43e6e741f51e594083c37dde590c6a779a2
Tignaman88 <alessioscarpello2809@gmail.com>
2024-08-11 08:13:52 +0100

    Computer Score Label Added

--- RockPaperScissorProject/src/RockPaperScissorGUI.java
+++ RockPaperScissorProject/src/RockPaperScissorGUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 //fronted
 public class RockPaperScissorGUI extends JFrame {
@@ -15,8 +16,27 @@ public class RockPaperScissorGUI extends JFrame {
 
         //load GUI in the centre of the screen every time we run the application
         setLocationRelativeTo(null);
+
+        //add GUI components
+        addGuiComponents();
     }
 
+    private void addGuiComponents(){
+        //create computer score label
+        JLabel computerScoreLabel = new JLabel("Computer: 0");
 
+        //set x,y coordinates and width/height alues
+        computerScoreLabel.setBounds(0, 43, 450, 30);
+
+        //set the font to have a font family of dialog, font-weight of bold, and a font-size of 26
+        computerScoreLabel.setFont(new Font("Dialog", Font.BOLD, 26));
+
+        //place the text in the centre
+        computerScoreLabel.setHorizontalAlignment(SwingConstants.CENTER);
+
+        //add to GUI
+        add(computerScoreLabel);
+        ;
+    }
 
 }

7c8a8ed7a51b3f8ec51a871e2afecce04951d37a
Tignaman88 <alessioscarpello2809@gmail.com>
2024-08-11 08:08:20 +0100

    Creating And Launching The GUI In The Centre
    Launching GUI in the centre of the screen

--- RockPaperScissorProject/src/RockPaperScissorGUI.java
+++ RockPaperScissorProject/src/RockPaperScissorGUI.java
@@ -1,2 +1,22 @@
-public class RockPaperScissorGUI {
+import javax.swing.*;
+
+//fronted
+public class RockPaperScissorGUI extends JFrame {
+    public RockPaperScissorGUI(){
+        //invoke iframe constructor and add title to the GUI
+        super("Rock Paper Scissor");
+
+        //set the size of th GUI
+        setSize(450, 574);
+
+        //set layout to null to disable layout management, so we can use absolute positioning for the elements
+        //(i.e. setting x,y coordinates and width/height values)
+        setLayout(null);
+
+        //load GUI in the centre of the screen every time we run the application
+        setLocationRelativeTo(null);
+    }
+
+
+
 }
